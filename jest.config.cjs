7a8c9b5e42b41d69431616275fe409339828e9ef
migberto <146127140+migberto@users.noreply.github.com>
2026-08-26 18:38:26 +0000

    Remove the Sentry protocol repository contents

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: '.',
-  testMatch: ['<rootDir>/src/**/*.spec.ts'],
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts'],
-  clearMocks: true,
-};
